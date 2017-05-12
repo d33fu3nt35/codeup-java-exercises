772f10774cd274e2955fc97d977d6420e96af825
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-05-12 11:36:36 -0500

    Basic Arithmetic Lesson - Added a method that makes the user put a number in valid range.

--- src/BasicArithmetic.java
+++ src/BasicArithmetic.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
+
 /**
  * Created by daniel on 5/11/17.
  */
 public class BasicArithmetic {
     public static void main(String[] args) {
-
+        double min = 1;
+        double max = 15;
         System.out.println("ADDITION");
 //        ADDITION
-        double x = 1;
-        double y = 9;
+        double x = getdouble(min, max);
+        double y = getdouble(min, max);
         double addanswer = addition(x, y);
         System.out.println(addanswer);
         addanswer = addition(20, 5);
@@ -16,9 +19,9 @@ public class BasicArithmetic {
         System.out.println("SUBTRACTION");
 //        SUBTRACTION
         double subanswer;
-        x = 5;
-        y = 4;
-        subanswer = subtraction (x, y);
+        x = getdouble(min, max);
+        y = getdouble(min, max);
+        subanswer = subtraction(x, y);
         System.out.println(subanswer);
         subanswer = subtraction(10, 5);
         System.out.println(subanswer);
@@ -26,9 +29,9 @@ public class BasicArithmetic {
         System.out.println("DIVISION");
 //        DIVISION
         double divanswer;
-        x = 10;
-        y = 5;
-        divanswer = division (x, y);
+        x = getdouble(min, max);
+        y = getdouble(min, max);
+        divanswer = division(x, y);
         System.out.println(divanswer);
         divanswer = division(20, 5);
         System.out.println(divanswer);
@@ -37,10 +40,15 @@ public class BasicArithmetic {
         System.out.println("MULTIPLICATION");
 //        MULTIPLICATION
         double multianswer;
-        x = 10;
-        y = 15;
-        multianswer = multiplication (x, y);
+        x = getdouble(min, max);
+        y = getdouble(min, max);
+        System.out.println("FOR LOOP");
+        multianswer = multiplication(x, y);
         System.out.println(multianswer);
+        System.out.println("RECURSION");
+        multianswer = multiplicationrecursion(x, y);
+        System.out.println(multianswer);
+        System.out.println("HARDCODE");
         multianswer = multiplication(20, 5);
         System.out.println(multianswer);
 
@@ -55,42 +63,63 @@ public class BasicArithmetic {
 //        }
 
 
-
         System.out.println("MODULUS");
         // MODULUS
         double modanswer;
-        x = 50;
-        y = 5;
-        modanswer = modulus (x, y);
+        x = getdouble(min, max);
+        y = getdouble(min, max);
+        modanswer = modulus(x, y);
         System.out.println(modanswer);
-        modanswer = modulus( 100, 10);
+        modanswer = modulus(100, 10);
         System.out.println(modanswer);
-
 
 
     }
 
-    public static double addition(double x, double y){
+    public static double getdouble(double min, double max) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number between " + min + " & " + max);
+        double userInput = scan.nextDouble();
+        if (userInput >= min && userInput <= max) {
+            return userInput;
+        } else{
+            System.out.println("Invalid number, try again.");
+            return getdouble(min, max);
+        }
+    }
+
+    public static double addition(double x, double y) {
         return x + y;
     }
 
-    public static double subtraction(double x, double y){
+    public static double subtraction(double x, double y) {
         return x - y;
     }
 
-    public static double division(double x, double y){
+    public static double division(double x, double y) {
         return x / y;
     }
 
-    public static double multiplication(double x, double y){
+    public static double multiplication(double x, double y) {
         double multianswer = 0;
-        for (int counter = 1; counter <= y; counter++){
+        for (int counter = 1; counter <= y; counter++) {
             multianswer = multianswer + x;
         }
         return multianswer;
     }
 
-    public static double modulus(double x, double y){
+    public static double multiplicationrecursion(double x, double y) {
+        if (y == 0) {
+            return 1;
+        } else if (y == 1) {
+            return x;
+        } else if (y == 2) {
+            return x * x;
+        }
+        return x * multiplicationrecursion(x, y - 1);
+    }
+
+    public static double modulus(double x, double y) {
         return x % y;
     }
 }
